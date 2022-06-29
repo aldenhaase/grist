b8941e6e3e75f09117826ef1e750af0183eea68d
aldenhaase <aldenhaase@gmail.com>
2022-06-29 11:13:18 -0600

    run script is working now

--- bin/run.sh
+++ bin/run.sh
@@ -6,14 +6,17 @@ ng build --delete-output-path true
 cd ../server/
 echo "Starting Server"
 go run main.go & 
-_pid=$!
+_pid_run=$!
+sleep 1
+_pid_server=$(ps aux|grep '[g]o-build'|awk '{print $2}'|tr -d '\n')
 echo "Server Running..."
 
 echo "Press any key to kill"
 while [ true ] ; do
-read -t 3 -n 1
+read -s -t 3 -n 1
 if [ $? = 0 ] ; then
-kill $_pid;
+kill $_pid_run ;
+kill $_pid_server ;
 exit ;
 fi
 done
