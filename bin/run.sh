644ffc4f6a67053a74114845d3e3a8944ef0ffc2
aldenhaase <aldenhaase@gmail.com>
2022-06-28 18:07:52 -0600

    setup angular etc

---
+++ bin/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Building Client"
+cd ../client/
+ng build --delete-output-path true --output-path ../server/dist
+cd ../server/
+echo "Starting Server"
+go run main.go & 
+_pid=$!
+echo "Server Running..."
+
+echo "Press any key to kill"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+kill $_pid;
+exit ;
+fi
+done
---
+++ server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.Handle("/", http.FileServer(http.Dir("./dist/")))
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
