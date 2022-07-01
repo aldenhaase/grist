9664fb67d93e8a90e1b84f1631edbedb0dfec480
aldenhaase <aldenhaase@gmail.com>
2022-07-01 12:22:55 -0600

    build script updates

--- bin/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ..
-gcloud builds submit --region=us-west2 --config cloudbuild.yaml .
--- bin/local_deploy.sh
+++ bin/local_deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd ..
-cloud-build-local --config=cloudbuild.yaml --dryrun=false --push=false  --write-workspace=../ .
+cd ..
+rm -rf workspace/
+cd lystr
+cloud-build-local --config=cloudbuild/local_cloudbuild.yaml --dryrun=false --push=false  --write-workspace=../ .
+
+/usr/bin/python2.7 /usr/lib/google-cloud-sdk/bin/dev_appserver.py --application=lystr-354722 go-app/app.yaml
