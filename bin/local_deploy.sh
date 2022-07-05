f68ec2b9d790cd90563a06e92c53d9e41a70a646
aldenhaase <aldenhaase@gmail.com>
2022-07-05 15:45:34 -0600

    docker naming

--- bin/local_deploy.sh
+++ bin/local_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --no-cache -t temp_build -f ../dockerbuild/Dockerfile ../
-docker image rm temp_build
+
+cloud-build-local --config="../cloudbuild/build.yaml" --dryrun=false --push=false ../
