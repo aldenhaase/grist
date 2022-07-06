d66e1fdf0415a2ef4f4b76ab2a0f1f39bde11a5e
aldenhaase <aldenhaase@gmail.com>
2022-07-06 16:37:21 -0600

    no need to export path on new docker image

--- cloudbuild/exports.sh
+++ cloudbuild/exports.sh
@@ -3,4 +3,3 @@ echo export APP_LOCATION=\"/workspace/\" >> /root/.bashrc
 echo export APP_CONFIG_LOCATION=\"/workspace/app_config/\" >> /root/.bashrc
 echo export SERVER_SOURCE_LOCATION=\"/workspace/src/server/src/\" >> /root/.bashrc
 echo export CLIENT_SOURCE_LOCATION=\"/workspace/src/client/\" >> /root/.bashrc
-echo export PATH=$PATH:/usr/local/go/bin:/root/.nvm/versions/node/v16.15.1/bin >> /root/.bashrc
