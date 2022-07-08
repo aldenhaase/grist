a5c7386472567e00f167936dad816d327d3bf4de
aldenhaase <aldenhaase@gmail.com>
2022-07-08 14:28:23 -0600

    reading post body

--- src/server/src/main.go
+++ src/server/src/main.go
@@ -4,10 +4,29 @@ import (
     "log"
     "net/http"
     "os"
+    "io"
+    "fmt"
+    "strings"
 )
 
 func Test(num int)int{
     return num;
+}
+
+type logLine struct{
+    UserIP string `json:"user_ip"`
+    Event string `json:"event"`
+}
+
+func login(w http.ResponseWriter, req *http.Request){
+    buf := new(strings.Builder)
+    io.Copy(buf, req.Body)
+    fmt.Fprintf(w,buf.String())
+}
+
+func setupHandlers(mux *http.ServeMux){
+    mux.Handle("/", http.FileServer(http.Dir("dist/")))
+    mux.HandleFunc("/login", login)
 }
 
 func main() {
@@ -17,9 +36,8 @@ func main() {
         port = "8080"
         log.Printf("Defaulting to port %s", port)
     }
-
-    path := "dist/"
-    http.Handle("/", http.FileServer(http.Dir(path)))
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+    mux := http.NewServeMux();
+    setupHandlers(mux)
+    log.Fatal(http.ListenAndServe(":"+port, mux))
 
 }
